df6529938e349cdc2495b8a77df796963c78bb14
Oliver <oliver.burleigh@atos.net>
2019-09-12 07:01:37 +0100

    add copy overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import './ColorBox.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {copied: false};
+  }
+
+  changeCopyState = () => {
+    this.setState({copied: true}, () => 
+      setTimeout(() => this.setState({copied:false}), 1500))
+  }
   render() {
     const {name, background} = this.props;
+    const { copied } = this.state;
     return (
-      <div style={{background}} className="ColorBox">
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{background}} className="ColorBox">
+        <div style={{background}} className={`copy-overlay ${copied && "show"}`} />
+        <div className={`copy-msg ${copied && "show"}`}>
+        <h1>Copied!</h1>
+        <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
           <span>{name}</span>
@@ -14,6 +31,7 @@ export default class ColorBox extends Component {
         </div>
         <span className="see-more">More</span>
       </div>
+      </CopyToClipboard>
     )
   }
 }
